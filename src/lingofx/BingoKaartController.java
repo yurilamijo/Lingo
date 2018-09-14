8059656604be557068d95d411b6dded11482f4a8
NtheGamer <nielsvanschaik14@gmail.com>
2018-09-14 10:48:30 +0200

    added bingo kaart

---
+++ src/lingofx/BingoKaartController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lingofx;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author niels_000
+ */
+public class BingoKaartController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    @FXML
+    private void kaartButtonAction(ActionEvent event){
+        for(int i = 0; i<25; i=i+1){
+            int randomNum = ThreadLocalRandom.current().nextInt(1, 70 + 1);
+        }
+    }
+}
