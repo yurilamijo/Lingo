1a2d4fc9521685a67f78e8bd7ca4dd8260cf7883
NtheGamer <nielsvanschaik14@gmail.com>
2018-09-20 14:57:56 +0200

    array is now showing the numbers on the form

--- src/lingofx/BingoKaartController.java
+++ src/lingofx/BingoKaartController.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -26,11 +27,40 @@ public class BingoKaartController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-    public int[] kaart1 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    @FXML
-    private void kaartButtonAction(ActionEvent event) {
+    public int[] kaart1 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    
+    @FXML private Label kaartNum1;
+    @FXML private Label kaartNum2;
+    @FXML private Label kaartNum3;
+    @FXML private Label kaartNum4;
+    @FXML private Label kaartNum5;
+    @FXML private Label kaartNum6;
+    @FXML private Label kaartNum7;
+    @FXML private Label kaartNum8;
+    @FXML private Label kaartNum9;
+    @FXML private Label kaartNum10;
+    @FXML private Label kaartNum11;
+    @FXML private Label kaartNum12;
+    @FXML private Label kaartNum13;
+    @FXML private Label kaartNum14;
+    @FXML private Label kaartNum15;
+    @FXML private Label kaartNum16;
+    @FXML private Label kaartNum17;
+    @FXML private Label kaartNum18;
+    @FXML private Label kaartNum19;
+    @FXML private Label kaartNum20;
+    @FXML private Label kaartNum21;
+    @FXML private Label kaartNum22;
+    @FXML private Label kaartNum23;
+    @FXML private Label kaartNum24;
+    @FXML private Label kaartNum25;
+    
+    @FXML private void kaartButtonAction(ActionEvent event) {
         GenerateKaart1();
+        PutNumInLabel(kaart1);
+        
+        
     }
 
     public void GenerateKaart1() {
@@ -63,5 +93,33 @@ public class BingoKaartController implements Initializable {
                 kaart1[i] = randomNum;
             }
         }
+    }
+    
+    public void PutNumInLabel(int[] arr){
+        kaartNum1.setText(Integer.toString(arr[0]));
+        kaartNum2.setText(Integer.toString(arr[1]));
+        kaartNum3.setText(Integer.toString(arr[2]));
+        kaartNum4.setText(Integer.toString(arr[3]));
+        kaartNum5.setText(Integer.toString(arr[4]));
+        kaartNum6.setText(Integer.toString(arr[5]));
+        kaartNum7.setText(Integer.toString(arr[6]));
+        kaartNum8.setText(Integer.toString(arr[7]));
+        kaartNum9.setText(Integer.toString(arr[8]));
+        kaartNum10.setText(Integer.toString(arr[9]));
+        kaartNum11.setText(Integer.toString(arr[10]));
+        kaartNum12.setText(Integer.toString(arr[11]));
+        kaartNum13.setText(Integer.toString(arr[12]));
+        kaartNum14.setText(Integer.toString(arr[13]));
+        kaartNum15.setText(Integer.toString(arr[14]));
+        kaartNum16.setText(Integer.toString(arr[15]));
+        kaartNum17.setText(Integer.toString(arr[16]));
+        kaartNum18.setText(Integer.toString(arr[17]));
+        kaartNum19.setText(Integer.toString(arr[18]));
+        kaartNum20.setText(Integer.toString(arr[19]));
+        kaartNum21.setText(Integer.toString(arr[20]));
+        kaartNum22.setText(Integer.toString(arr[21]));
+        kaartNum23.setText(Integer.toString(arr[22]));
+        kaartNum24.setText(Integer.toString(arr[23]));
+        kaartNum25.setText(Integer.toString(arr[24]));
     }
 }
