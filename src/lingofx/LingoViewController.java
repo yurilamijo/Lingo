ae89912c6061f9a303c0f4fff6006ca36f023a94
Yuri Lamijo <yurilamijo@hotmail.com>
2018-10-02 14:47:51 +0200

    Lingo woord gets checked and board change on check

--- src/lingofx/LingoViewController.java
+++ src/lingofx/LingoViewController.java
@@ -10,10 +10,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
@@ -26,10 +26,12 @@ import javafx.scene.text.Font;
 public class LingoViewController implements Initializable {
 
     @FXML private Label labelName;
+    @FXML private Label message;
+    @FXML private Label timer;
     @FXML private TextField woordInput;
-    @FXML private Label woord;
     @FXML private GridPane grid;
     
+    private StackPane stackpane; 
     private String lingoWoord = "fiets";
     private int turn = 0;
     private final int maxValue = 5;
@@ -41,83 +43,75 @@ public class LingoViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         buildBoard();
+        labels[turn][0].setText(String.valueOf(lingoWoord.charAt(0)));
     }    
     
     public void setName(String name){
         labelName.setText(name);
     }
     
-    @FXML
-    private void sendWoordAction(ActionEvent event) {
+    private void timer(){
         
+    }
+    
+    @FXML
+    private void sendWoordAction(ActionEvent event) {      
         if (woordInput.getText().matches(".*\\d+.*") || woordInput.getText().length() != 5) {
-            System.out.println("numbers");
             if (woordInput.getText().length() != 5) {
-                woord.setText("Woord moet 5 letters lang zijn");
+                message.setText("Woord moet 5 letters lang zijn");
             } else {
-                woord.setText("Incorrect value");
+                message.setText("Incorrect value");
             }
         } else {
             if(turn < 5){
-                woord.setText(woordInput.getText());
-//              charNum.setText(String.valueOf(wordInput.getText().length()));
                 changeBoard();
                 turn++;
-                
-//                System.out.println("Letters" + Arrays.deepToString(woorden));
             } else {
-                woord.setText("Max turns");
+                message.setText("Max turns");
             }
         }
-
         woordInput.clear();
     }
     
     private void buildBoard(){
         for(int i=0;i<5;i++){          
             for(int j=0;j<5;j++){
-                Circle circle = new Circle(0,0,30,Color.GRAY);
-                circles[i][j] = circle;
-                grid.setHalignment(circle, HPos.CENTER);
-                grid.add(circle, j, i); 
-            }   
-        }
-        
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                Label label = new Label();
+                stackpane = new StackPane();  
+                Circle circle = new Circle(0,0,30,Color.GRAY);                             
+                Label label = new Label();          
                 label.setFont(new Font("Arial", 30));
+                
+                circles[i][j] = circle;
                 labels[i][j] = label;
-                grid.setHalignment(label, HPos.CENTER);
-                grid.add(label, j, i);
-            }
+                
+                stackpane.getChildren().add(circle);                
+                stackpane.getChildren().add(label);
+                grid.add(stackpane, j, i);
+            }   
         }
     }
     
     private void changeBoard(){
-        changeLetter(turn);
-    }
-    
-    private void changeLetter(int turn){
         for(int i=0;i<labels[turn].length;i++){
             String letter = String.valueOf(woordInput.getText().charAt(i));
-            woorden[turn] = woordInput.getText();
             Label letterLabel = labels[turn][i];
+            
+            woorden[turn] = woordInput.getText();
             letterLabel.setText(letter);
             
             checkWoord(i);
         }
-        System.out.println("\n");
     }
-    
-    private void checkWoord(int letters){
+        
+    private void checkWoord(int index){
+        char woord = woorden[turn].charAt(index);
         // Check if letters are in lingoWoord
-        if(lingoWoord.indexOf(woorden[turn].charAt(letters)) != -1){
-            System.out.println("woord contains letter but wrong position: " + woorden[turn].charAt(letters));
-            
+        if(lingoWoord.indexOf(woord) != -1){
             // Check if letter is same position
-            if(String.valueOf(lingoWoord.charAt(letters)).contains(String.valueOf(woorden[turn].charAt(letters)))){
-                System.out.println(woorden[turn].charAt(letters) + " Same position");
+            if(String.valueOf(lingoWoord.charAt(index)).contains(String.valueOf(woord))){
+                circles[turn][lingoWoord.indexOf(woord)].setFill(Color.GREEN);
+            } else {
+                circles[turn][woorden[turn].indexOf(woord)].setFill(Color.YELLOW);
             }
         }
     }
